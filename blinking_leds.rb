1ca80aa8ad3529a3647433362cb8149f7b2be7d2
Klaus Beyer <xkbeyer@gmail.com>
2017-01-03 19:59:49 +0100

    Enhanced version with more oop and meta programming.

--- blinking_leds.rb
+++ blinking_leds.rb
@@ -13,66 +13,84 @@
 #                  djnz <labelref>
 #  
 
-def setLED(bits)
-  outs = (0..7).map { |i|
-    ((1<< i) & bits) > 0 ? '*' : '.'
-  }.reverse.join
-  puts "#{outs}"
+class Processor
+  attr_accessor :line
+  def initialize
+    @line = 0
+    @labels = Hash.new
+    @bits = 0
+    @loops = 0
+  end
+  def ld(register, value)
+    if register == 'a'
+      @bits = value.to_i
+    elsif register == 'b'
+      @loops = value.to_i
+    else
+      puts "Invalid register #{register}"
+    end
+    @line += 1
+  end
+  def out(port, value)
+    puts "out #{port},#{value}" if $debug
+    setLED
+    @line += 1
+  end
+  def rlca(*ignore)
+    puts "rlca" if $debug
+    @line += 1
+    @bits = ((@bits << 1) | ((@bits & 0x80) >> 7)) & 0xFF
+  end
+  def rrca(*ignore)
+    puts "rrca" if $debug
+    @line += 1
+    @bits = ((@bits >> 1) | ((@bits & 0x01) << 7)) & 0xff
+  end
+  def djnz(label, *ignore)
+    puts "dnjz #{label}" if $debug
+    @loops -= 1
+    if @loops > 0
+      @line = @labels[label.strip] - 1
+    else
+      @loops = 0
+    end
+    @line += 1
+  end
+  def addLabel(label)
+    puts "Add label '#{label}' at line #{@line}" if $debug
+    @labels[label.strip] = @line;
+    @line += 1
+  end
+  def setLED()
+    outs = (0..7).map { |i|
+      ((1<< i) & @bits) > 0 ? '*' : '.'
+    }.reverse.join
+    puts "#{outs}"
+  end
+  def nop
+    @line += 1
+  end
 end
 
-def rlca(bits)
-  ((bits << 1) | ((bits & 0x80) >> 7)) & 0xFF
-end
-
-def rrca(bits)
-  ((bits >> 1) | ((bits & 0x01) << 7)) & 0xff
-end
-
-regld   = /\s?ld\s+a\,\s*(\d{1,3})/
-regout  = /\s?out\s+\(0\),\s*a/
-reglabel= /([a-z]+):\s*/
-regrlca = /\s+rlca\s*/
-regrrca = /\s+rrca\s*/
-regloop = /\s+djnz\s+([a-z]*)\s*/
-regldb  = /\s?ld\s+b\,\s*(\d{1,3})/
+reg = /\s*(ld|out|rlca|rrca|djnz)\s*([a-z]+|a|b|\(0\))?(\s*,?\s*(\d{1,3}|a|b))?\s*/
 
 InputFileName = ARGV[0]
 text = File.open(InputFileName,"r").readlines()
 
-bits = nil
-i = 0
-labels = Hash.new
-loops = 0
-
-while i < text.length do
-  m = text[i].match regld
-  bits = m[1].to_i if m
-  if (text[i].match regout) != nil 
-    setLED bits
-  end
-  if (text[i].match regrlca) != nil
-    bits = rlca(bits)
-  end
-  if (text[i].match regrrca) != nil
-    bits = rrca(bits)
-  end
-  m = text[i].match reglabel
-  if m != nil
-    labels[m[1].strip] = i
-  end
-  m = text[i].match regloop
-  if m != nil
-    loops -= 1
-    if loops > 0
-      i = labels[m[1].strip] - 1
+vm = Processor.new
+while vm.line < text.length do
+  l = text[vm.line]
+  m = (l.scan reg).flatten
+  if m.count > 0 
+    vm.method(m[0]).call(m[1],m[3])
+  else
+    m = (l.scan /([a-z]+):\s*/).flatten
+    puts "#{l} #{m}" if $debug
+    if m[0]
+      vm.addLabel(m[0])
     else
-      loops = 0
+      vm.nop
     end
   end
-  m = text[i].match regldb
-  if m != nil
-    loops = m[1].to_i
-  end
-  i += 1
 end
 
